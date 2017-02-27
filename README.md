d21151a06f6964b51e8c9b62fd0e0d0a52018f2b
Alec Jacobson <alecjacobson@gmail.com>
2017-02-27 00:14:02 -0500

    working biharmonic defo example

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-# geometry-processing-deformation
-Deformation assignment for Geometry Processing course
+# Geometry Processing – Deformation
+
+> **To get started:** Fork this repository then issue
+> 
+>     git clone --recursive http://github.com/[username]/geometry-processing-deformation.git
+>
+
+## Installation, Layout, and Compilation
+
+See
+[introduction](http://github.com/alecjacobson/geometry-processing-introduction).
+
+## Execution
+
+Once built, you can execute the assignment from inside the `build/` by running
+on a given mesh:
+
+    ./deformation [path to mesh.obj]
+
+## Background
+
+### Linear Methods
+
+Measure distortion with respect to small displacements
+
+#### Gradient-based energy
+
+- Dependent on global coordinate system
+
+- Not smooth at constraints 
+
+- Each coordinate of "displacement field" is a harmonic function (picture of
+  arrows and picture of x-coordinate)
+  - Each coordinate is a heat diffusion toward some average 
+  - As-_constant_-as-possible
+
+#### Laplacian-based energy
+
+- Each coordinate of "displacement field" is a biharmonic function 
+  - larger space of functions than harmonic
+  - includes functions that are smooth at the constraints
+  - As-_harmonic_-as-possible (not simply harmonic since we have to satisfy more
+    constraints): locally harmonic roughly corresponds to locally
+    "fair"/non-ripply (technically, harmonic means bending in one direction
+    equals opposite amount of bending in the other direction. [Saddle]()
+    shaped)
+
+- Corresponds to higher-order interpolation à la Hermite splines 
+
+Dependent on local coordinate system, but still confused by large rotations.
+
+#### $k$-harmonic
+
+The logical continuation of harmonic and biharmonic deformation is to consider
+triharmonic and tetraharmonic and so on. It's straightforward to extend these,
+though there are diminishing returns and increasing costs.
+
+### Non-Linear Methods
+
+Take a step back. Why didn't we like the gradient based energy? Global
+coordinate frame, non-smoothness at constraints. Put aside non-smoothness at
+constraints. Let's try to tackle large rotations.
+
+**Deformation gradient** for 2D shapes
+
+#### As-rigid-as-possible energy
+
+Warm-up: For 2D shapes
+
+> ##### Volumes
+
+##### Surfaces
+
+Overlapping integration regions
+
+## Tasks
+
+### Blacklist
+
+ - `igl::harmonic`
+ - `igl::arap`
+ - `igl::covariance_scatter_matrix`
+
+### Whitelist
+
+ - `igl::fit_rigid` (or your previous assignment's `closest_rotation`)
+
+### k-harmonic precomputation
+
+### k-harmonic deformation
+
+### covariance scatter matrix
+
+### arap precompuation
+
+### arap deformation
---
+++ main.cpp
@@ -0,0 +1,219 @@
+#include <igl/read_triangle_mesh.h>
+#include <igl/viewer/Viewer.h>
+#include <igl/project.h>
+#include <igl/unproject.h>
+#include <igl/snap_points.h>
+#include <igl/slice.h>
+#include <igl/harmonic.h>
+#include <igl/unproject_onto_mesh.h>
+#include <Eigen/Core>
+#include <string>
+#include <iostream>
+#include <stack>
+
+struct State
+{
+  Eigen::MatrixXd CV;
+  Eigen::MatrixXd CU;
+  bool placing_handles = true;
+} s;
+
+int main(int argc, char *argv[])
+{
+  // Undo Management
+  std::stack<State> undo_stack,redo_stack;
+  const auto push_undo = [&](State & _s=s)
+  {
+    undo_stack.push(_s);
+    // clear
+    redo_stack = std::stack<State>();
+  };
+  const auto undo = [&]()
+  {
+    if(!undo_stack.empty())
+    {
+      redo_stack.push(s);
+      s = undo_stack.top();
+      undo_stack.pop();
+    }
+  };
+  const auto redo = [&]()
+  {
+    if(!redo_stack.empty())
+    {
+      undo_stack.push(s);
+      s = redo_stack.top();
+      redo_stack.pop();
+    }
+  };
+
+  Eigen::MatrixXd V,U;
+  Eigen::MatrixXi F;
+  long sel = -1;
+  Eigen::RowVector3f last;
+
+  // Load input meshes
+  igl::read_triangle_mesh(
+    (argc>1?argv[1]:"../shared/data/bunny.off"),V,F);
+  U = V;
+  // Load data into MatrixXd rather than VectorXd for simpler `smooth` API
+  // Just use y-coordinates as data to be smoothed
+  // Create a libigl Viewer object to toggle between point cloud and mesh
+  igl::viewer::Viewer viewer;
+  std::cout<<R"(
+[space]  Toggle whether placing handles or deforming
+)";
+
+  bool plot_parameterization = false;
+  const auto & update = [&]()
+  {
+    using namespace igl;
+    if(s.placing_handles)
+    {
+      viewer.data.set_vertices(V);
+      viewer.data.set_colors(
+        Eigen::RowVector3d(GOLD_DIFFUSE[0], GOLD_DIFFUSE[1], GOLD_DIFFUSE[2]));
+      viewer.data.set_points(s.CV,Eigen::RowVector3d(1.0,0.5,0.1));
+    }else
+    {
+
+      Eigen::VectorXi b;
+      igl::snap_points(s.CV,V,b);
+      Eigen::MatrixXd D;
+      igl::harmonic(V,F,b,s.CU-s.CV,2, D);
+      U = V+D;
+      viewer.data.set_vertices(U);
+      viewer.data.set_colors(
+        Eigen::RowVector3d(SILVER_DIFFUSE[0], SILVER_DIFFUSE[1], SILVER_DIFFUSE[2]));
+      viewer.data.set_points(s.CU,Eigen::RowVector3d(1.0,0.5,0.1));
+    }
+    viewer.data.compute_normals();
+  };
+  viewer.callback_mouse_down = 
+    [&](igl::viewer::Viewer&, int, int)->bool
+  {
+    last = Eigen::RowVector3f(
+      viewer.current_mouse_x,
+      viewer.core.viewport(3) - viewer.current_mouse_y,
+      0);
+    if(s.placing_handles)
+    {
+      int fid;
+      Eigen::Vector3f bc;
+      // Cast a ray in the view direction starting from the mouse position
+      if(igl::unproject_onto_mesh(
+        last.head(2),
+        viewer.core.view * viewer.core.model,
+        viewer.core.proj, 
+        viewer.core.viewport, 
+        V, F, 
+        fid, bc))
+      {
+        push_undo();
+        s.CV.conservativeResize(s.CV.rows()+1,3);
+        s.CV.row(s.CV.rows()-1) = 
+          V.row(F(fid,0))*bc(0)+ V.row(F(fid,1))*bc(1)+ V.row(F(fid,2))*bc(2);
+        update();
+        return true;
+      }
+    }else
+    {
+      // Get closest control point
+      Eigen::MatrixXf CP;
+      igl::project(
+        Eigen::MatrixXf(s.CU.cast<float>()),
+        viewer.core.view * viewer.core.model, 
+        viewer.core.proj, viewer.core.viewport, CP);
+      Eigen::VectorXf D = (CP.rowwise()-last).rowwise().norm();
+      sel = (D.minCoeff(&sel) < 30)?sel:-1;
+      if(sel != -1)
+      {
+        last(2) = CP(sel,2);
+        push_undo();
+        update();
+        return true;
+      }
+    }
+    return false;
+  };
+
+  viewer.callback_mouse_move = [&](igl::viewer::Viewer &, int,int)->bool
+  {
+    if(sel!=-1)
+    {
+      Eigen::RowVector3f drag(
+        viewer.current_mouse_x,
+        viewer.core.viewport(3) - viewer.current_mouse_y,
+        last(2));
+      Eigen::RowVector3f drag_scene,last_scene;
+      igl::unproject(drag, viewer.core.view * viewer.core.model, viewer.core.proj,viewer.core.viewport,drag_scene);
+      igl::unproject(last, viewer.core.view * viewer.core.model, viewer.core.proj,viewer.core.viewport,last_scene);
+      s.CU.row(sel) += (drag_scene-last_scene).cast<double>();
+      last = drag;
+
+      update();
+      return true;
+    }
+    return false;
+  };
+  viewer.callback_mouse_up = [&](igl::viewer::Viewer&, int, int)->bool
+  {
+    sel = -1;
+    return false;
+  };
+  viewer.callback_key_pressed = 
+    [&](igl::viewer::Viewer &, unsigned int key, int mod)
+  {
+    switch(key)
+    {
+      case 'R':
+      case 'r':
+      {
+        push_undo();
+        s.CU = s.CV;
+        break;
+      }
+      case ' ':
+        push_undo();
+        s.placing_handles ^= 1;
+        if(!s.placing_handles)
+        {
+          s.CU = s.CV;
+        }
+        break;
+      default:
+        return false;
+    }
+    update();
+    return true;
+  };
+
+  viewer.callback_key_down = 
+    [&](igl::viewer::Viewer &, unsigned char key, int mod)->bool
+  {
+    switch(key)
+    {
+      case 'Z':
+        if(mod & GLFW_MOD_SUPER)
+        {
+          if(mod & GLFW_MOD_SHIFT)
+          {
+            redo();
+          }else
+          {
+            undo();
+          }
+        }
+        update();
+        break;
+    }
+    return false;
+  };
+  viewer.data.set_mesh(V,F);
+  viewer.core.show_lines = false;
+  viewer.core.is_animating = true;
+  update();
+  viewer.launch();
+
+  return EXIT_SUCCESS;
+}
