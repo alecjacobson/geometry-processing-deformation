5ec341bf63bb5b8c91cf6df32b905ccaeda94697
rkevingibson <rkevingibson@gmail.com>
2017-03-10 17:39:59 -0500

    Biharmonic method working.

--- src/arap_precompute.cpp
+++ src/arap_precompute.cpp
@@ -1,11 +1,33 @@
 #include "arap_precompute.h"
 
+#include <igl/cotmatrix.h>
+#include <igl/cotmatrix_entries.h>
+#include <igl/min_quad_with_fixed.h>
+
 void arap_precompute(
-  const Eigen::MatrixXd & V,
-  const Eigen::MatrixXi & F,
-  const Eigen::VectorXi & b,
-  igl::min_quad_with_fixed_data<double> & data,
-  Eigen::SparseMatrix<double> & K)
+	const Eigen::MatrixXd & V,
+	const Eigen::MatrixXi & F,
+	const Eigen::VectorXi & b,
+	igl::min_quad_with_fixed_data<double> & data,
+	Eigen::SparseMatrix<double> & K)
 {
-  // REPLACE WITH YOUR CODE
+
+	//Start by doing the precomputation to generate data.
+
+	Eigen::SparseMatrix<double> L;
+	igl::cotmatrix(V, F, L);
+
+	Eigen::SparseMatrix<double> Aeq(0, 0);
+	igl::min_quad_with_fixed_precompute(L, b, Aeq, false, data);
+	
+	//Next, construct K.
+	
+	int num_faces = F.rows();
+	for (int f = 0; f < num_faces; f++) {
+		for (int v = 0; v < 3; v++) {
+			Eigen::RowVector3f edge = V.row(F(f, (v + 1) % 3)) - V.row(F(f, v));
+
+		}
+	}
+
 }
--- src/biharmonic_solve.cpp
+++ src/biharmonic_solve.cpp
@@ -2,11 +2,20 @@
 #include <igl/min_quad_with_fixed.h>
 
 void biharmonic_solve(
-  const igl::min_quad_with_fixed_data<double> & data,
-  const Eigen::MatrixXd & bc,
-  Eigen::MatrixXd & D)
+	const igl::min_quad_with_fixed_data<double> & data,
+	const Eigen::MatrixXd & bc,
+	Eigen::MatrixXd & D)
 {
   // REPLACE WITH YOUR CODE
-  D = Eigen::MatrixXd::Zero(data.n,3);
+	
+	int n = data.n;
+	Eigen::MatrixXd B = Eigen::MatrixXd::Zero(n, 1);
+	Eigen::MatrixXd Beq = Eigen::MatrixXd::Zero(0,0);
+	D.setZero(n, 3);
+	for (int i = 0; i < 3; i++) {
+		Eigen::VectorXd d;
+		igl::min_quad_with_fixed_solve(data, B, bc.col(i), Beq, d);
+		D.col(i) = d;
+	}
 }
 
