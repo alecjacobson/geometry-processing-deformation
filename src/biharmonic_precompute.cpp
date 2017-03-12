cb4cd1b71b924c17db1978fc38f7076fad59c4f7
Rinat Abdrashitov <rindopuz23@gmail.com>
2017-03-12 13:32:10 -0400

    started workgin on it

--- src/biharmonic_precompute.cpp
+++ src/biharmonic_precompute.cpp
@@ -1,5 +1,10 @@
 #include "biharmonic_precompute.h"
 #include <igl/min_quad_with_fixed.h>
+#include <igl/cotmatrix.h>
+#include <igl/massmatrix.h>
+
+using namespace Eigen;
+using namespace igl;
 
 void biharmonic_precompute(
   const Eigen::MatrixXd & V,
@@ -8,6 +13,25 @@ void biharmonic_precompute(
   igl::min_quad_with_fixed_data<double> & data)
 {
   // REPLACE WITH YOUR CODE
-  data.n = V.rows();
+  //data.n = V.rows();
+	
+	SparseMatrix<double> L;
+	cotmatrix(V, F, L);
+
+	SparseMatrix<double> M;	
+	massmatrix(V, F, MASSMATRIX_TYPE_DEFAULT, M);
+
+	//Computer M inverse
+	SimplicialLDLT <SparseMatrix<double> > solver;
+	solver.compute(M);
+	SparseMatrix<double> I(M.rows(), M.cols());
+	I.setIdentity();
+	SparseMatrix<double> M_inv = solver.solve(I);
+
+	SparseMatrix<double> Q = L.transpose() * M_inv * L;
+
+	Eigen::SparseMatrix<double> Aeq;
+	min_quad_with_fixed_data<double> data;
+	min_quad_with_fixed_precompute(Q, b, Aeq, false, data);
 }
 
--- src/biharmonic_solve.cpp
+++ src/biharmonic_solve.cpp
@@ -1,12 +1,18 @@
 #include "biharmonic_solve.h"
 #include <igl/min_quad_with_fixed.h>
 
+using namespace Eigen;
+using namespace igl;
+
 void biharmonic_solve(
   const igl::min_quad_with_fixed_data<double> & data,
   const Eigen::MatrixXd & bc,
   Eigen::MatrixXd & D)
 {
   // REPLACE WITH YOUR CODE
-  D = Eigen::MatrixXd::Zero(data.n,3);
+  // D = Eigen::MatrixXd::Zero(data.n,3);
+	
+	MatrixXd Du = data.ldlt.solve
+
 }
 
