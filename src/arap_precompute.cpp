8f3c09355ce443af0317bb0de80984377555872c
M-Anwar <muhammedshabbeer@hotmail.com>
2017-03-13 02:46:06 -0400

    Finally got ARAP working
    - ARAP working

--- src/arap_precompute.cpp
+++ src/arap_precompute.cpp
@@ -9,5 +9,40 @@ void arap_precompute(
   igl::min_quad_with_fixed_data<double> & data,
   Eigen::SparseMatrix<double> & K)
 {
-  
+	int N = V.rows();
+
+	Eigen::SparseMatrix<double> L;
+	igl::cotmatrix(V, F, L);
+	Eigen::MatrixXd cot(F.rows(), 3);
+	igl::cotmatrix_entries(V, F, cot);
+
+	K.resize(N, 3 * N);
+	std::vector<Eigen::Triplet<double>> triplets;	
+
+	for (int f = 0; f < F.rows(); f++)
+	{
+		for (int e = 0; e < 3; e++)
+		{
+			int k = F(f, e);
+
+			for (int d = 0; d < 3;d++) {
+				triplets.emplace_back(F(f, 1), 3 * k + d, (cot(f, 0) / 3.0) * (V(F(f,1), d) - V(F(f,2), d)));
+				triplets.emplace_back(F(f, 2), 3 * k + d, (cot(f, 0) / 3.0) * (V(F(f, 2), d) - V(F(f, 1), d)));
+			}
+
+			for (int d = 0; d < 3;d++) {
+				triplets.emplace_back(F(f, 2), 3 * k + d, (cot(f, 1) / 3.0) * (V(F(f, 2), d) - V(F(f, 0), d)));
+				triplets.emplace_back(F(f, 0), 3 * k + d, (cot(f, 1) / 3.0) * (V(F(f, 0), d) - V(F(f, 2), d)));
+			}
+
+			for (int d = 0; d < 3;d++) {
+				triplets.emplace_back(F(f, 0), 3 * k + d, (cot(f, 2) / 3.0) * (V(F(f, 0), d) - V(F(f, 1), d)));
+				triplets.emplace_back(F(f, 1), 3 * k + d, (cot(f, 2) / 3.0) * (V(F(f, 1), d) - V(F(f, 0), d)));
+			}						
+		}
+	}
+
+	K.setFromTriplets(triplets.begin(), triplets.end());
+		
+	igl::min_quad_with_fixed_precompute(L, b, Eigen::SparseMatrix<double>(), false, data);
 }
--- src/arap_single_iteration.cpp
+++ src/arap_single_iteration.cpp
@@ -7,6 +7,20 @@ void arap_single_iteration(
   const Eigen::SparseMatrix<double> & K,
   const Eigen::MatrixXd & bc,
   Eigen::MatrixXd & U)
-{
-  
+{	
+	Eigen::MatrixXd  C = (U.transpose()*K).transpose();
+
+	//Local Step
+	Eigen::MatrixXd R(3 * U.rows(), 3);
+	Eigen::Matrix3d cov, rot;
+	for (int i = 0; i < U.rows(); i++) {
+		cov = C.block(i*3, 0, 3, 3);		
+		igl::polar_svd3x3<Eigen::Matrix3d>(cov, rot);
+		R.block(i * 3, 0, 3, 3) = rot; //Eigen::Matrix3d::Identity();
+	}
+
+	//Global Step
+	Eigen::MatrixXd B = K * R;
+	igl::min_quad_with_fixed_solve(data, B, bc, Eigen::MatrixXd(), U);
+	
 }
