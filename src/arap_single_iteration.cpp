0ea02f4b858cc0896d4de9df5c01e24ea1919eb6
Timothy Jeruzalski <timjj22@gmail.com>
2017-03-13 21:04:49 -0400

    Biharmonic works great, but the ARAP has a weird scaling bug

--- src/arap_single_iteration.cpp
+++ src/arap_single_iteration.cpp
@@ -1,4 +1,6 @@
 #include "arap_single_iteration.h"
+#include <igl/min_quad_with_fixed.h>
+#include <igl/polar_svd3x3.h>
 
 void arap_single_iteration(
   const igl::min_quad_with_fixed_data<double> & data,
@@ -6,5 +8,41 @@ void arap_single_iteration(
   const Eigen::MatrixXd & bc,
   Eigen::MatrixXd & U)
 {
-  // REPLACE WITH YOUR CODE
+  /*
+    minimizing the following problem,
+
+    tr(VᵀLV) + tr(VᵀKR)
+
+    in a local-global optimization, where R is updated keeping V constant then another step where R is kept constant and V is updated
+
+    C = (VᵀK)ᵀ
+    which are the stacked covariance matrices, allow for the computation of the R_k from the closest rotation to C_k
+   */
+  // U are the current positions of the vertices
+  
+  // The rotation matrices are 3x3  
+  Eigen::MatrixXd R(3*U.rows(), 3);
+  Eigen::MatrixXd C = K.transpose() * U;
+  
+  // solve for the R matrix, with the closest rotation
+  for(int32_t k = 0; k < U.rows(); k++)
+  {
+    Eigen::Matrix3d r_k;
+    // cut out the C_k submatrices to form the nearest rotation for R_k:
+    Eigen::Matrix3d c_k = C.block(3*k, 0, 3, 3);
+    igl::polar_svd3x3<Eigen::Matrix3d>(c_k, r_k);
+    R.block(3*k, 0, 3, 3) = r_k;
+  }
+
+  // solve for the tr(VᵀLV), each xyz separate
+  Eigen::MatrixXd KR = (K*R);
+  for(int32_t dim = 0; dim < 3; dim++)
+  {
+    Eigen::VectorXd fixed = bc.col(dim);
+    Eigen::VectorXd B = KR.col(dim);
+    Eigen::VectorXd beq, Z;
+    igl::min_quad_with_fixed_solve(data, B, fixed, beq, Z);
+
+    U.col(dim) = Z;
+  }
 }
--- src/biharmonic_solve.cpp
+++ src/biharmonic_solve.cpp
@@ -6,7 +6,27 @@ void biharmonic_solve(
   const Eigen::MatrixXd & bc,
   Eigen::MatrixXd & D)
 {
-  // REPLACE WITH YOUR CODE
-  D = Eigen::MatrixXd::Zero(data.n,3);
+  /*
+    min_x ∫ ‖∆d‖² dA ≈ tr(DᵀLᵀM⁻ᵀMM⁻¹LD) = tr(DᵀLᵀM⁻¹LD) = tr(DᵀQD)
+    
+    For which Q has already been prefactorized for our problem, and so we just need to perform the solve with our given handle locations
+
+    The bc specifies the (x,y,z) location of the point which is fixed, and due to the minimization of the trace, solve each X,Y,Z indep:
+   */
+
+  D = Eigen::MatrixXd::Zero(data.n, 3);
+  
+  // size of the system is data.n
+  Eigen::MatrixXd B = Eigen::MatrixXd::Zero(data.n, 1);
+  Eigen::MatrixXd Beq = Eigen::MatrixXd::Zero(0,0);
+
+  // solve for the (X, Y, Z)'s separately
+  for(int32_t i = 0; i < 3; i++)
+  {
+    Eigen::VectorXd dcol;
+    if(!igl::min_quad_with_fixed_solve(data, B, bc.col(i), Beq, dcol))
+      printf("ERROR mqwf compute\n");
+    D.col(i) = dcol;
+  }
 }
 
