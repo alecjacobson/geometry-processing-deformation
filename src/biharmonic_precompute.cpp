8c5606f317aee48cbf79ff65148700f111c42bf9
Michelle <m.arkhangorodsky@gmail.com>
2018-03-02 19:21:15 -0500

    started biharmonic_precompute.cpp

--- src/biharmonic_precompute.cpp
+++ src/biharmonic_precompute.cpp
@@ -1,4 +1,6 @@
 #include "biharmonic_precompute.h"
+#include <igl/cotmatrix.h>
+#include <igl/massmatrix.h>
 #include <igl/min_quad_with_fixed.h>
 
 void biharmonic_precompute(
@@ -8,6 +10,30 @@ void biharmonic_precompute(
   igl::min_quad_with_fixed_data<double> & data)
 {
   // REPLACE WITH YOUR CODE
-  data.n = V.rows();
-}
+  //data.n = V.rows();
 
+  // Get Laplacian
+ Eigen::SparseMatrix<double> L;
+ igl::cotmatrix(V,F,L);
+ // Get Mass mastrix
+ Eigen::SparseMatrix<double> M;
+ igl::massmatrix(V,F,igl::MASSMATRIX_TYPE_DEFAULT,M);
+ // Get inverse of mass Matrix
+ Eigen::SimplicialLLT<Eigen::SparseMatrix<double> > solver;
+ solver.compute(M);
+ Eigen::SparseMatrix<double> I(M.rows(),M.rows());
+ I.setIdentity();
+ auto M_inv = solver.solve(I);
+
+ // Get Q
+ Eigen::MatrixXd Q;
+ Q = L.transpose() * M_inv * L;
+ Eigen::SparseMatrix<double> Q_sparse;
+ Q_sparse = Q.sparseView();
+
+ // Empty constraints
+ Eigen::SparseMatrix<double> Aeq;
+
+ // Minimize energey trace( 0.5*D'*Q*D + U'*B + constant )
+ igl::min_quad_with_fixed_precompute(Q_sparse, b, Aeq, false, data);
+}
