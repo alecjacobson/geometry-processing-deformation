17f63056f241c037a1db63f83bb254f4716fce17
Sarah Kushner <sarahannekushner@yahoo.com>
2018-03-05 03:33:51 -0500

    biharmonic solve done, working on arap

--- src/arap_precompute.cpp
+++ src/arap_precompute.cpp
@@ -1,4 +1,7 @@
 #include "arap_precompute.h"
+#include <igl/min_quad_with_fixed.h>
+#include <igl/cotmatrix.h>
+#include <igl/massmatrix.h>
 
 void arap_precompute(
   const Eigen::MatrixXd & V,
@@ -7,5 +10,52 @@ void arap_precompute(
   igl::min_quad_with_fixed_data<double> & data,
   Eigen::SparseMatrix<double> & K)
 {
-  // REPLACE WITH YOUR CODE
+	int n = V.rows();
+	int m = F.rows();
+	int k = b.rows();
+
+	Eigen::SparseMatrix<double> L(n, n);
+	igl::cotmatrix(V, F, L);
+
+	/*
+	Eigen::SparseMatrix<double> M(n, n);
+	igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_BARYCENTRIC, M);
+
+	Eigen::SparseMatrix<double> Q = L.transpose() * M.cwiseInverse() * L;
+	*/
+
+	Eigen::SparseMatrix<double> Aeq;
+
+	igl::min_quad_with_fixed_precompute(L, b, Aeq, false, data);
+
+	typedef Eigen::Triplet<double> T;
+	std::vector<T> tripletList;
+	tripletList.reserve(n);
+
+	K.resize(n, 3*n);
+	for (int f = 0; f < m; f++)
+	{
+		auto face = F.row(f);
+		for (int k = 0; k < 3; k++)
+		{
+			int i = face(k);
+			int j = 0;
+			if (k + 1 < 3)
+				j = face(k + 1);
+			else
+				j = face(0);
+
+			Eigen::Vector3d v_i = V.row(i);
+			Eigen::Vector3d v_j = V.row(j);
+			Eigen::Vector3d e_ij = L.coeff(i,j)*(v_i-v_j);
+			for (int beta = 0; beta < 3; beta++)
+			{
+				tripletList.push_back(T(i, 3*k + beta, e_ij(beta)));
+				tripletList.push_back(T(j, 3*k + beta, -e_ij(beta)));
+			}
+		}
+	}
+
+	K.setFromTriplets(tripletList.begin(), tripletList.end());
+	//K = V.transpose() * K;
 }
--- src/arap_single_iteration.cpp
+++ src/arap_single_iteration.cpp
@@ -1,4 +1,7 @@
 #include "arap_single_iteration.h"
+#include <igl/min_quad_with_fixed.h>
+#include <igl/polar_svd3x3.h>
+#include <Eigen/Dense>
 
 void arap_single_iteration(
   const igl::min_quad_with_fixed_data<double> & data,
@@ -6,5 +9,21 @@ void arap_single_iteration(
   const Eigen::MatrixXd & bc,
   Eigen::MatrixXd & U)
 {
-  // REPLACE WITH YOUR CODE
+  Eigen::MatrixXd C = K; //bc.transpose() * K;
+
+  Eigen::JacobiSVD<Eigen::MatrixXd> svd(C, Eigen::ComputeFullU | Eigen::ComputeFullV);
+  Eigen::Matrix3d v = svd.matrixV();
+  Eigen::Matrix3d u = svd.matrixU();
+  Eigen::Matrix3d omega;
+  Eigen::Matrix3d UVt = u * v.transpose();
+  omega << 1, 0, 0,
+		0, 1, 0,
+		0, 0, UVt.determinant();
+	
+  Eigen::MatrixXd R = u * omega * v.transpose();
+
+  Eigen::VectorXd Beq;
+  Eigen::VectorXd B = K*R;
+
+  igl::min_quad_with_fixed_solve(data, B, bc, Beq, U);
 }
