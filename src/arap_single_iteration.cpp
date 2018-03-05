a4262f98cd86c61f7108564b0a1346b29ca2c74d
Yanjun7J <y7jiang@gmail.com>
2018-03-05 17:48:39 -0500

    A5

--- src/arap_single_iteration.cpp
+++ src/arap_single_iteration.cpp
@@ -1,4 +1,6 @@
 #include "arap_single_iteration.h"
+#include <igl/polar_svd3x3.h>
+#include <igl/min_quad_with_fixed.h>
 
 void arap_single_iteration(
   const igl::min_quad_with_fixed_data<double> & data,
@@ -7,4 +9,20 @@ void arap_single_iteration(
   Eigen::MatrixXd & U)
 {
   // REPLACE WITH YOUR CODE
+	// Compute closest rotation R
+	Eigen::MatrixXd C_T = U.transpose()*K;
+	Eigen::MatrixXd R(3 * U.rows(), 3);
+	for (int k = 0; k < U.rows(); k++) {
+		Eigen::Matrix3d C_k = C_T.transpose().block<3, 3>(3 * k, 0);
+		C_k = C_k / C_k.maxCoeff(); // Normalize C_k
+		Eigen::Matrix3d R_k;
+		igl::polar_svd3x3(C_k, R_k);
+		R.block<3, 3>(3 * k, 0) = R_k;
+	}
+	// Compute B=KR
+	Eigen::MatrixXd B = K * R;
+
+	// Compute single arap minimization
+	Eigen::VectorXd Beq;
+	igl::min_quad_with_fixed_solve(data, B, bc, Beq, U);
 }
--- src/biharmonic_precompute.cpp
+++ src/biharmonic_precompute.cpp
@@ -1,5 +1,7 @@
 #include "biharmonic_precompute.h"
 #include <igl/min_quad_with_fixed.h>
+#include <igl/cotmatrix.h>
+#include <igl/massmatrix.h>
 
 void biharmonic_precompute(
   const Eigen::MatrixXd & V,
@@ -8,6 +10,37 @@ void biharmonic_precompute(
   igl::min_quad_with_fixed_data<double> & data)
 {
   // REPLACE WITH YOUR CODE
-  data.n = V.rows();
+	// Compute cotangent laplacian for the mesh
+	typedef Eigen::SparseMatrix<double> SpMat;
+	SpMat L;
+	igl::cotmatrix(V, F, L);
+
+	// Compute mass matrix for the mesh
+	SpMat M;
+	igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_DEFAULT, M);
+
+	// Compute M^-1
+	// Method 1: since M is a diagonal matrix, M^-1 = (1.0/M.diagonal()).asdiagonal
+	Eigen::VectorXd diagonal = 1.0/(M.diagonal().array());
+	SpMat M_inv;
+	M_inv.resize(M.rows(), M.cols());
+	for (int i = 0; i < M_inv.rows(); i++) {
+		M_inv.insert(i, i) = diagonal[i];
+	}
+	// Method 2: solve M^1-M = I (didn't see much difference in performance with small meshes but it doesn't seem to
+	// work with large meshes like knight.off, don't know why...)
+	/*
+	SpMat I(M.rows(), M.cols());
+	I.setIdentity();
+	Eigen::SimplicialCholesky<SpMat> chol(M);
+	SpMat M_inv = chol.solve(I);
+	*/
+	
+	// Compute Q = L^TM^-1L
+	SpMat Q = L.transpose()*M_inv*L;
+
+	// Precompute for biharmonic deformation
+	SpMat Aeq;
+	igl::min_quad_with_fixed_precompute(Q, b, Aeq, false, data);
 }
 
