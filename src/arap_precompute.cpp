42021dc2a6d6e05a9edfcac7119c0acba263fea3
sharm146 <shash.sharma@mail.utoronto.ca>
2018-03-06 14:32:52 -0500

    Assignment complete (I think...)

--- src/arap_precompute.cpp
+++ src/arap_precompute.cpp
@@ -1,4 +1,7 @@
 #include "arap_precompute.h"
+#include <igl/cotmatrix_entries.h>
+#include <igl/cotmatrix.h>
+#include <igl/min_quad_with_fixed.h>
 
 void arap_precompute(
   const Eigen::MatrixXd & V,
@@ -8,4 +11,83 @@ void arap_precompute(
   Eigen::SparseMatrix<double> & K)
 {
   // REPLACE WITH YOUR CODE
+
+	// First, do the factorization on the laplacian matrix, which corresponds to the first term in
+	// the global minimization function.
+
+	// Get cotangent laplacian
+	Eigen::SparseMatrix<double> L;
+	igl::cotmatrix(V, F, L);
+
+	// Do precomputation, same as in the biharmonic case
+	Eigen::SparseMatrix<double> Aeq;
+
+	// Factorize
+	igl::min_quad_with_fixed_precompute(L, b, Aeq, false, data);
+
+
+	// We know we're going to need the cotangents of angles that appear in the cotengent laplacian.
+	// Looks like igl has a function for this...
+
+	Eigen::MatrixXd Cij (F.rows(), 3);
+	igl::cotmatrix_entries(V, F, Cij);
+
+	// Initialize triplets that will be used to fill the K matrix later
+	typedef Eigen::Triplet<double> T;
+	std::vector<T> triplets;
+
+	std::vector<int> V_set_1 = {1, 2, 0}, V_set_2 = {2, 0, 1};
+
+	// As suggested in the readme, first loop though all faces
+	for (int ii = 0; ii < F.rows(); ii++)
+	{
+		// Extract the vertices on this face
+		Eigen::VectorXi V_ii = F.row(ii);
+
+		// Now loop over each edge on this face
+		for (int jj = 0; jj < 3; jj++)
+		{
+			// Extract the vertices of this edge, but in the order that agrees with the order
+			// that cotmatrix_entries returns them in. So, first time round we need vertices (1,2),
+			// second time we need (2,0), and then finally we need (0,1). To make this easier I made
+			// the vectors V_set_1 and V_set_2 above to extract the right vertices easily.
+			int Vi = V_ii(V_set_1[jj]);
+			int Vj = V_ii(V_set_2[jj]);
+
+			// Compute the difference between these
+			Eigen::VectorXd V_diff = V.row(Vi) - V.row(Vj);
+
+			// Extract this extry in the cotangent entries
+			double cij = Cij(ii, jj);
+
+			// Compute the product of these two to enter into K. Since the cotangent entries returned
+			// are scaled by 1/2 already, we need to further divide by 3 to get the 1/6 factor needed.
+			Eigen::VectorXd e = cij*V_diff/3.0;
+
+			// The alpha loop
+			for (int alpha = 0; alpha < 3; alpha++)
+			{
+				// Extract this vertex
+				int V_alpha = F(ii, alpha);
+
+				// The beta loop
+				for (int beta = 0; beta < 3; beta++)
+				{
+					// Insert these entries as per readme
+					triplets.push_back(T(Vi, 3*V_alpha + beta, e(beta)));
+					triplets.push_back(T(Vj, 3*V_alpha + beta, -e(beta)));
+
+					// Note: I'm hoping that if multiple values are inserted into the same K entry
+					// via triplets, they'll just add up...potential place to debug
+				}
+			} 
+		}
+	}
+
+	// Create K matrix
+	K.resize(V.rows(), 3*V.rows());
+	K.setFromTriplets(triplets.begin(), triplets.end());
+
+	return;
+
 }
--- src/biharmonic_precompute.cpp
+++ src/biharmonic_precompute.cpp
@@ -1,5 +1,7 @@
 #include "biharmonic_precompute.h"
 #include <igl/min_quad_with_fixed.h>
+#include <igl/cotmatrix.h>
+#include <igl/massmatrix.h>
 
 void biharmonic_precompute(
   const Eigen::MatrixXd & V,
@@ -7,7 +9,45 @@ void biharmonic_precompute(
   const Eigen::VectorXi & b,
   igl::min_quad_with_fixed_data<double> & data)
 {
-  // REPLACE WITH YOUR CODE
-  data.n = V.rows();
+  // // REPLACE WITH YOUR CODE
+  // data.n = V.rows();
+
+	// We want to factorize the the matrix that corresponds to Q in the readme. This requires
+	// first computing the cotangent laplacian and mass matrices.
+
+	// Get cotangent laplacian
+	Eigen::SparseMatrix<double> L;
+	igl::cotmatrix(V, F, L);
+
+	// Get mass matrix
+	Eigen::SparseMatrix<double> M;
+	igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_DEFAULT, M);
+
+	// Compute the inverse of the mass matrix. Since we're using the diagonalized version,
+	// all we have to do is compute reciprocals along the diagonal.
+	Eigen::SparseMatrix<double> M_inv(M.cols(), M.cols());
+	typedef Eigen::Triplet<double> T;
+	std::vector<T> triplets;
+
+	for (int ii = 0; ii < M.cols(); ii++)
+		triplets.push_back(T(ii, ii, 1.0/M.coeff(ii, ii)));
+
+	M_inv.setFromTriplets(triplets.begin(), triplets.end());
+
+	// Compute Q
+	Eigen::SparseMatrix<double> Q (L.cols(), L.cols());
+	Q = L.transpose()*M_inv*L;
+	// std::cout << "Here" << std::endl;
+
+	// Factorization
+
+	// Create empty matrices for the equality constraints, of which we have none (because the known vertices are separate inputs)
+	Eigen::SparseMatrix<double> Aeq;
+
+	// Factorize
+	igl::min_quad_with_fixed_precompute(Q, b, Aeq, false, data);
+
+	return;
+
 }
 
