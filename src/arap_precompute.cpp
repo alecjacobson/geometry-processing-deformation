329e67856a63c3dc4029f0a77d0de21b4825a526
KENAN DENG <dknyxh@gmail.com>
2018-03-06 05:05:47 -0500

    Fix error, still not right

--- src/arap_precompute.cpp
+++ src/arap_precompute.cpp
@@ -2,6 +2,7 @@
 #include <igl/cotmatrix.h>
 #include <igl/cotmatrix_entries.h>
 #include <igl/min_quad_with_fixed.h>
+#include <iostream>
 void arap_precompute(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
@@ -16,29 +17,33 @@ void arap_precompute(
   Eigen::MatrixXd e;
   igl::cotmatrix_entries(V,F,e);
   //[1, 2] [2, 0] [0, 1]
-  
+  std::cout << "Here" << std::endl;
+  std::cout << e.cols() << e.rows() << std::endl;
   for (int row = 0; row < F.rows(); row++){
   	//iterate through each edge
-  	for (int col = 0; col = F.cols(); col++){
+  	for (int col = 0; col < F.cols(); col++){
   		int i = F(row, col);
   		int j = F(row, (col+1)%F.cols());
-		int k_p[3] = {i, j , (col+2)%3};
+  		int k_p[3];
+      k_p[0] = i;
+      k_p[1] = j;
+      k_p[2] = F(row, (col+2)%F.cols());
+      std::cout << "1" << std::endl;
+  		Eigen::MatrixXd e_ij = e(row, (i+2)%F.cols()) * (V.row(i) - V.row(j)).transpose();
 		
-		
-		Eigen::MatrixXd e_ij = e(row, (i+2)%F.cols()) * (V.row(i) - V.row(j)).transpose();
-		
-		for (int k_i = 0; k_i < 3; k_i++)
-		{
-			int k = k_p[k_i];
-			for (int beta = 1; beta < 4; beta++){
-		  		K.coeffRef(i, 3 * k + beta) = e_ij(beta - 1, 0);
-		  		K.coeffRef(j, 3 * k + beta) = - e_ij(beta - 1, 0);
+  		for (int k_i = 0; k_i < 3; k_i++)
+  		{
+  			int k = k_p[k_i];
+			  for (int beta = 0; beta < 3; beta++){
+		  		K.coeffRef(i, 3 * k + beta) += e_ij(beta , 0);
+		  		K.coeffRef(j, 3 * k + beta) += - e_ij(beta , 0);
 	  		}
 	  	}
   	}
+    std::cout << "2" << std::endl;
   	
   }
-  
+  std::cout << "Here" << std::endl;
   Eigen::SparseMatrix<double> L;
   igl::cotmatrix(V, F, L);
   Eigen::SparseMatrix<double> dummy(0,0);
