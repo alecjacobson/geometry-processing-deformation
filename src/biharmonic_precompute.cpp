32d6794c4dbb9216dedc86bb52624c21ea3f9b62
Ziheng Liang <ziheng941008@hotmail.com>
2018-03-05 22:03:20 -0500

    finished biharmonic

--- src/biharmonic_precompute.cpp
+++ src/biharmonic_precompute.cpp
@@ -1,5 +1,7 @@
 #include "biharmonic_precompute.h"
 #include <igl/min_quad_with_fixed.h>
+#include <igl/cotmatrix.h>
+#include <iostream>
 
 void biharmonic_precompute(
   const Eigen::MatrixXd & V,
@@ -8,6 +10,8 @@ void biharmonic_precompute(
   igl::min_quad_with_fixed_data<double> & data)
 {
   // REPLACE WITH YOUR CODE
-  data.n = V.rows();
+  Eigen::SparseMatrix<double> A; 
+  igl::cotmatrix(V, F, A);
+  min_quad_with_fixed_precompute(A, b, Eigen::SparseMatrix<double>(), false, data);
 }
 
--- src/biharmonic_solve.cpp
+++ src/biharmonic_solve.cpp
@@ -1,5 +1,6 @@
 #include "biharmonic_solve.h"
 #include <igl/min_quad_with_fixed.h>
+#include <iostream>
 
 void biharmonic_solve(
   const igl::min_quad_with_fixed_data<double> & data,
@@ -7,6 +8,15 @@ void biharmonic_solve(
   Eigen::MatrixXd & D)
 {
   // REPLACE WITH YOUR CODE
-  D = Eigen::MatrixXd::Zero(data.n,3);
+  Eigen::MatrixXd Dx;
+  Eigen::MatrixXd Dy;
+  Eigen::MatrixXd Dz;
+  min_quad_with_fixed_solve(data,Eigen::MatrixXd::Zero(data.n,1),bc.col(0),Eigen::MatrixXd(), Dx);
+  min_quad_with_fixed_solve(data,Eigen::MatrixXd::Zero(data.n,1),bc.col(1),Eigen::MatrixXd(), Dy);
+  min_quad_with_fixed_solve(data,Eigen::MatrixXd::Zero(data.n,1),bc.col(2),Eigen::MatrixXd(), Dz);
+  D = Eigen::MatrixXd::Zero(data.n, 3);
+  D.col(0) = Dx;
+  D.col(1) = Dy;
+  D.col(2) = Dz;
 }
 
