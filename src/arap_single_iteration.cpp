ba9905e8e23db8a06dc0706bb9a50baa18563c7d
Mark <peiqi1122@gmail.com>
2018-10-21 04:09:34 -0400

    add

--- src/arap_single_iteration.cpp
+++ src/arap_single_iteration.cpp
@@ -1,10 +1,29 @@
 #include "arap_single_iteration.h"
 
+#include <igl/polar_svd3x3.h>
+#include <igl/min_quad_with_fixed.h>
+
 void arap_single_iteration(
   const igl::min_quad_with_fixed_data<double> & data,
   const Eigen::SparseMatrix<double> & K,
   const Eigen::MatrixXd & bc,
   Eigen::MatrixXd & U)
 {
-  // REPLACE WITH YOUR CODE
+    // Local step: find locally optimal rotation matrices R
+
+    const int n = data.n;
+    Eigen::MatrixXd R(3*n, 3); // a stack of rotation 3x3 matrix for each vertex
+    Eigen::MatrixXd C = K.transpose() * U;
+    Eigen::Matrix3d m, r;
+
+    for (int k = 0; k < n; ++k) {
+        m = C.block(3*k, 0, 3, 3);
+        igl::polar_svd3x3(m, r);
+        R.block(3*k, 0, 3, 3) = r;
+    }
+
+    // Global step: find distortion minimizing vertex positions U
+
+    Eigen::MatrixXd Beq;
+    igl::min_quad_with_fixed_solve(data, K*R, bc, Beq, U);
 }
--- src/biharmonic_precompute.cpp
+++ src/biharmonic_precompute.cpp
@@ -1,4 +1,7 @@
 #include "biharmonic_precompute.h"
+
+#include <igl/cotmatrix.h>
+#include <igl/massmatrix.h>
 #include <igl/min_quad_with_fixed.h>
 
 void biharmonic_precompute(
@@ -6,8 +9,19 @@ void biharmonic_precompute(
   const Eigen::MatrixXi & F,
   const Eigen::VectorXi & b,
   igl::min_quad_with_fixed_data<double> & data)
-{
-  // REPLACE WITH YOUR CODE
-  data.n = V.rows();
-}
+{    
+    int nv = V.rows();
+    Eigen::SparseMatrix<double> L, M, I, Q, Aeq;
+    igl::cotmatrix(V, F, L);
+    igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_DEFAULT, M);
 
+    I.resize(nv, nv);
+    I.setIdentity();
+    Eigen::SimplicialLLT<Eigen::SparseMatrix<double>> solver;
+    M = solver.compute(M).solve(I);
+
+    Q.resize(nv, nv);
+    Q = L.transpose() * M * L;
+
+    igl::min_quad_with_fixed_precompute(Q, b, Aeq, false, data);
+}
