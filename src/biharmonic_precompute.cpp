c815dca4d91795f61a66816d55bd595136ee2f47
Adrian <adrian.she@alumni.ubc.ca>
2018-10-21 18:28:28 -0400

    biharmonic is done

--- src/biharmonic_precompute.cpp
+++ src/biharmonic_precompute.cpp
@@ -1,5 +1,8 @@
 #include "biharmonic_precompute.h"
+#include <Eigen/SparseCholesky>
 #include <igl/min_quad_with_fixed.h>
+#include <igl/cotmatrix.h>
+#include <igl/massmatrix.h>
 
 void biharmonic_precompute(
   const Eigen::MatrixXd & V,
@@ -8,6 +11,25 @@ void biharmonic_precompute(
   igl::min_quad_with_fixed_data<double> & data)
 {
   // REPLACE WITH YOUR CODE
-  data.n = V.rows();
+
+  // Construct mass and Laplacian Matrices
+  Eigen::SparseMatrix<double> L;
+  Eigen::SparseMatrix<double> M;
+  igl::massmatrix(V, F, igl::MassMatrixType::MASSMATRIX_TYPE_BARYCENTRIC, M);
+  igl::cotmatrix(V, F, L);
+  
+  // Compute M^{-1}
+  Eigen::SparseMatrix<double> I(V.rows(), V.rows());
+  I.setIdentity();
+  Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> ldlt(M);
+  Eigen::SparseMatrix<double> inv_M = ldlt.solve(M); 
+
+  // Calculate Q = L^T * M^{-1} * L
+  Eigen::SparseMatrix<double> Q = L.transpose() * inv_M * L; 
+
+  // Precompute the matrix
+  Eigen::SparseMatrix<double> A;
+  A.setZero(); 
+  igl::min_quad_with_fixed_precompute(Q, b, A, false, data);
 }
 
--- src/biharmonic_solve.cpp
+++ src/biharmonic_solve.cpp
@@ -7,6 +7,20 @@ void biharmonic_solve(
   Eigen::MatrixXd & D)
 {
   // REPLACE WITH YOUR CODE
-  D = Eigen::MatrixXd::Zero(data.n,3);
+
+  // Putting D^T Q D into the general quadratic minimization form min D^T A D + D^T B s.t. D(fixed_indices) = D_fixedvals, and A_eq x = Beq
+  // A in data, so B = A_eq = B_eq = 0;
+  D.resize(data.n, 3);
+  Eigen::VectorXd B(data.n);
+  B.setZero();
+  Eigen::VectorXd Beq;
+  Beq.setZero();
+
+  // Solve for the displacements in each coordinate
+  for (int i = 0; i < 3; i++) {
+  	Eigen::VectorXd cur_col; 
+	igl::min_quad_with_fixed_solve(data, B, bc.col(i), Beq, cur_col) ;
+	D.col(i) = cur_col;
+  }
 }
 
