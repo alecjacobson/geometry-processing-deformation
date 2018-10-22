5189fecfd4ff38e3ed1a71fb3fad4ab83f372197
Yihuan Mao <yihuan.mao@mail.utoronto.ca>
2018-10-21 21:03:09 -0400

    Submission

--- src/arap_precompute.cpp
+++ src/arap_precompute.cpp
@@ -1,4 +1,8 @@
 #include "arap_precompute.h"
+#include <igl/cotmatrix_entries.h>
+#include <igl/cotmatrix.h>
+#include <igl/min_quad_with_fixed.h>
+#include <fstream>
 
 void arap_precompute(
   const Eigen::MatrixXd & V,
@@ -7,5 +11,24 @@ void arap_precompute(
   igl::min_quad_with_fixed_data<double> & data,
   Eigen::SparseMatrix<double> & K)
 {
+  printf("start pre\n");
+  Eigen::MatrixXd L;
+  igl::cotmatrix_entries(V,F,L);
+  Eigen::SparseMatrix<double> LL;
+  igl::cotmatrix(V,F,LL);
+  igl::min_quad_with_fixed_precompute(LL,b,Eigen::SparseMatrix<double>(),false,data);
+  Eigen::RowVector3d tp;
+  K.resize(V.rows(),3*V.rows());
+  for (int i=0; i<F.rows(); i++){
+  	for (int j=0; j<3; j++){
+  	  tp=L(i,j)*(V.row(F(i,(j+1)%3))-V.row(F(i,(j+2)%3)));
+  	  for (int k=0; k<3; k++)
+  	  	for (int dim=0; dim<3; dim++){
+          K.coeffRef(F(i,(j+1)%3),3*F(i,k)+dim)+=tp(dim);
+          K.coeffRef(F(i,(j+2)%3),3*F(i,k)+dim)-=tp(dim);
+		}
+	}
+  }
   // REPLACE WITH YOUR CODE
+  printf("end pre\n");
 }
--- src/biharmonic_solve.cpp
+++ src/biharmonic_solve.cpp
@@ -1,12 +1,17 @@
 #include "biharmonic_solve.h"
 #include <igl/min_quad_with_fixed.h>
+#include <fstream>
 
 void biharmonic_solve(
   const igl::min_quad_with_fixed_data<double> & data,
   const Eigen::MatrixXd & bc,
   Eigen::MatrixXd & D)
 {
+  printf("start bisol\n");
+  const Eigen::VectorXd B_flat = Eigen::VectorXd::Zero(data.n);
+  igl::min_quad_with_fixed_solve(data,B_flat,bc,Eigen::VectorXd(),D);
   // REPLACE WITH YOUR CODE
-  D = Eigen::MatrixXd::Zero(data.n,3);
+  //D = Eigen::MatrixXd::Zero(data.n,3);
+  printf("end bisol\n");
 }
 
