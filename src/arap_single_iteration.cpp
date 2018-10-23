75df251e27b4d579102752c9d43651923ce26b26
eriszhang <jiayieris.zhang@mail.utoronto.ca>
2018-10-23 01:01:34 -0400

    finish arap

--- src/arap_single_iteration.cpp
+++ src/arap_single_iteration.cpp
@@ -1,4 +1,8 @@
 #include "arap_single_iteration.h"
+#include <igl/polar_svd3x3.h>
+#include <igl/min_quad_with_fixed.h>
+
+using namespace Eigen;
 
 void arap_single_iteration(
   const igl::min_quad_with_fixed_data<double> & data,
@@ -6,5 +10,17 @@ void arap_single_iteration(
   const Eigen::MatrixXd & bc,
   Eigen::MatrixXd & U)
 {
-  // REPLACE WITH YOUR CODE
+  // two level optimization: local and global
+  // construct C first
+  MatrixXd C = K.transpose() * U;
+  // construct R: a stack of rotation matrices
+  MatrixXd R(3 * data.n, 3); // a stack of rotation 3x3 matrix for each vertex
+  Matrix3d Ck, Rk;
+  for (int k = 0; k < data.n; k++) {
+    Ck = C.block(3*k, 0, 3, 3);
+    igl::polar_svd3x3(Ck, Rk);
+    R.block(3 * k, 0, 3, 3) = Rk;
+  }
+  // solve for new U
+  igl::min_quad_with_fixed_solve(data, K * R, bc, MatrixXd(), U);
 }
