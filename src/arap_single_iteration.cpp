e02b5ae1137fcb1ead2274c533e7787b3e17cac7
Adrian <adrian.she@alumni.ubc.ca>
2018-10-24 18:05:09 -0400

    remove imports that are unused

--- src/arap_single_iteration.cpp
+++ src/arap_single_iteration.cpp
@@ -1,8 +1,6 @@
 #include "arap_single_iteration.h"
 #include <igl/polar_svd.h>
 #include <igl/min_quad_with_fixed.h>
-#include <Eigen/LU>
-#include <Eigen/SVD>
 
 void arap_single_iteration(
   const igl::min_quad_with_fixed_data<double> & data,
