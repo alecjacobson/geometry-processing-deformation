411a81f8679e6599d5f745fbd330178aeb5eb920
Vineeth B <bhaskaravineeth@gmail.com>
2018-10-25 15:16:01 -0400

    Vineeth Bhaskara HW04 Submission

--- src/arap_precompute.cpp
+++ src/arap_precompute.cpp
@@ -1,4 +1,8 @@
 #include "arap_precompute.h"
+#include <vector>
+#include <igl/cotmatrix.h>
+#include <igl/min_quad_with_fixed.h>
+
 
 void arap_precompute(
   const Eigen::MatrixXd & V,
@@ -8,4 +12,64 @@ void arap_precompute(
   Eigen::SparseMatrix<double> & K)
 {
   // REPLACE WITH YOUR CODE
+
+  // Construct K
+  std::vector<Eigen::Triplet<double> > triplets;
+
+  // get the cotmatrix
+  Eigen::SparseMatrix<double> L;
+  igl::cotmatrix(V, F, L);
+
+  // iterate over faces
+  for (int f=0; f<F.rows(); f++) {
+
+    // iterate over 3 edges of a face using modulo
+    for (int e=0; e<3; e++) {
+      int v_i_idx = F(f, e);
+      int v_j_idx = F(f, (e + 1) % 3);
+
+      Eigen::Vector3d v_i = V.row(v_i_idx);
+      Eigen::Vector3d v_j = V.row(v_j_idx);
+
+      // v_i -> v_j represents edge e_ij
+      Eigen::Vector3d e_ij = (v_i - v_j) * L.coeff(v_i_idx, v_j_idx);
+
+      // loop over 3 dimensions x, y, z (beta)
+      for (int beta=0; beta<3; beta++) {
+        // index k can be index v_i or v_j or the third vertex
+        
+        // case 1: k is i 
+        int k_idx = v_i_idx; 
+        triplets.push_back(Eigen::Triplet<double>(v_i_idx, 3*k_idx + beta, (1.0/6.0)*e_ij(beta)));
+        triplets.push_back(Eigen::Triplet<double>(v_j_idx, 3*k_idx + beta, (-1.0/6.0)*e_ij(beta)));
+
+        // case 2: k is j
+        k_idx = v_j_idx; 
+        triplets.push_back(Eigen::Triplet<double>(v_i_idx, 3*k_idx + beta, (1.0/6.0)*e_ij(beta)));
+        triplets.push_back(Eigen::Triplet<double>(v_j_idx, 3*k_idx + beta, (-1.0/6.0)*e_ij(beta)));
+
+        // case 3: k i the third vertex
+        k_idx = F(f, (e + 2) % 3);
+        triplets.push_back(Eigen::Triplet<double>(v_i_idx, 3*k_idx + beta, (1.0/6.0)*e_ij(beta)));
+        triplets.push_back(Eigen::Triplet<double>(v_j_idx, 3*k_idx + beta, (-1.0/6.0)*e_ij(beta)));
+
+      }
+
+    }
+  }
+
+  K.resize(V.rows(), 3 * V.rows());
+  K.setFromTriplets(triplets.begin(), triplets.end());
+
+  // data matrix precomputed similar to biharmonic_precompute
+  // need to minimize_D tr(D'LD + D'KR)
+
+  // not using this 
+  Eigen::SparseMatrix<double> Aeq;
+
+  Eigen::SparseMatrix<double> L_times_2 = 2.0 * L;
+
+  // pd should be false else numerical issue as L is not positive definite
+  igl::min_quad_with_fixed_precompute(L_times_2, b, Aeq, false, data);
+
 }
--- src/biharmonic_precompute.cpp
+++ src/biharmonic_precompute.cpp
@@ -1,5 +1,8 @@
 #include "biharmonic_precompute.h"
 #include <igl/min_quad_with_fixed.h>
+#include <igl/cotmatrix.h>
+#include <igl/massmatrix.h>
+#include <vector>
 
 void biharmonic_precompute(
   const Eigen::MatrixXd & V,
@@ -9,5 +12,41 @@ void biharmonic_precompute(
 {
   // REPLACE WITH YOUR CODE
   data.n = V.rows();
+
+  Eigen::SparseMatrix<double> L;
+  igl::cotmatrix(V, F, L);
+
+  Eigen::SparseMatrix<double> M;
+  igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_BARYCENTRIC, M);
+
+  // Find the inverse of the Mass matrix. 
+  // Since it is a diagonal sparse,
+  // manually compute element by element and create M_inv
+  Eigen::SparseMatrix<double> M_inv;
+  M_inv.resize(M.rows(), M.cols());
+  std::vector<Eigen::Triplet<double> > triplets;
+
+  for (int j=0; j<M.rows(); j++) {
+    triplets.push_back(Eigen::Triplet<double>(j, j, 1.0/M.coeff(j,j)));
+  }
+
+  M_inv.setFromTriplets(triplets.begin(), triplets.end());
+
+  // multiply by 2.0 since the igl library uses a 1/2 in front of the
+  // quadratic term
+  Eigen::SparseMatrix<double> Q = 2.0 * L.transpose() * M_inv * L;
+
+  Eigen::SparseMatrix<double> Aeq;
+  //Aeq.resize(V.rows(), V.rows());
+
+  // Initially planned using Aeq and Beq below. But using Z_b, Z_bc is much simpler.
+  // // for all the handle points keep the diagonal entries in Aeq = 1
+  // std::vector<Eigen::Triplet<double> > Aeq_triplets;
+  // for (int j=0; j<b.size(); j++) {
+  //   Aeq_triplets.push_back(Eigen::Triplet<double>(b(j), b(j), 1.0));
+  // }
+  // Aeq.setFromTriplets(Aeq_triplets.begin(), Aeq_triplets.end());
+
+  igl::min_quad_with_fixed_precompute(Q, b, Aeq, false, data);
 }
 
