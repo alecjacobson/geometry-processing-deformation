4246514a96053c278cc0cd9348a0339390ee2fad
Feilong Song <feilong.song@mail.utoronto.ca>
2018-10-25 19:43:29 -0400

    hw4

--- src/biharmonic_precompute.cpp
+++ src/biharmonic_precompute.cpp
@@ -1,5 +1,7 @@
 #include "biharmonic_precompute.h"
 #include <igl/min_quad_with_fixed.h>
+#include <igl/cotmatrix.h>
+#include <igl/massmatrix.h>
 
 void biharmonic_precompute(
   const Eigen::MatrixXd & V,
@@ -7,7 +9,16 @@ void biharmonic_precompute(
   const Eigen::VectorXi & b,
   igl::min_quad_with_fixed_data<double> & data)
 {
-  // REPLACE WITH YOUR CODE
-  data.n = V.rows();
+  Eigen::SparseMatrix<double> L, M;
+  igl::cotmatrix(V, F, L);
+  igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_DEFAULT, M);
+
+  Eigen::SparseMatrix<double> M_i(M.rows(), M.cols());
+  for (int i = 0; i < M.rows(); i++) {
+    M_i.coeffRef(i, i) = 1.0 / M.coeff(i, i);
+  }
+
+  Eigen::SparseMatrix<double> Q = L.transpose() * M_i * L;
+  igl::min_quad_with_fixed_precompute(Q, b, Eigen::SparseMatrix<double>(), false, data);
 }
 
