8648f7e9ba0353edb400f04f884cc725f46d5fb5
Meng-Wei <mmeng.wei@mail.utoronto.ca>
2018-10-25 22:14:35 -0400

    finish

--- src/arap_single_iteration.cpp
+++ src/arap_single_iteration.cpp
@@ -1,4 +1,8 @@
 #include "arap_single_iteration.h"
+#include "biharmonic_precompute.h"
+#include <igl/min_quad_with_fixed.h>
+#include <iostream>
+#include <igl/polar_svd3x3.h>
 
 void arap_single_iteration(
   const igl::min_quad_with_fixed_data<double> & data,
@@ -6,5 +10,25 @@ void arap_single_iteration(
   const Eigen::MatrixXd & bc,
   Eigen::MatrixXd & U)
 {
-  // REPLACE WITH YOUR CODE
+  int N = U.rows();
+
+  // Construct C:
+  Eigen::MatrixXd C(3*N, 3);
+  C = (U.transpose() * K).transpose();
+
+  // Construct R:
+  Eigen::MatrixXd R(3*N, 3);
+  for(int i = 0; i < N; i ++) {
+    Eigen::Matrix3d ck, rk;
+    ck << C.row(i*3),
+          C.row(i*3 + 1),
+          C.row(i*3 + 2);
+    igl::polar_svd3x3(ck, rk);
+    R.row(i*3) = rk.row(0);
+    R.row(i*3 + 1) = rk.row(1);
+    R.row(i*3 + 2) = rk.row(2);
+  }
+  igl::min_quad_with_fixed_solve(data, K*R, bc, 
+                      Eigen::MatrixXd(), U);
+
 }
