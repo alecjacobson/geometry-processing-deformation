f5ba12ca9b7a366e996663a9312c41cbf1edde0a
jnyao <43320994+jnyao@users.noreply.github.com>
2018-10-25 21:02:50 -0400

    Update biharmonic_solve.cpp

--- src/biharmonic_solve.cpp
+++ src/biharmonic_solve.cpp
@@ -8,5 +8,8 @@ void biharmonic_solve(
 {
   // REPLACE WITH YOUR CODE
   D = Eigen::MatrixXd::Zero(data.n,3);
+  Eigen::MatrixXd B(data.n, data.n);
+  B.setZero();
+  Eigen::MatrixXd Beq;
+  igl::min_quad_with_fixed_solve(data, B, bc, Beq, D);
 }
-
