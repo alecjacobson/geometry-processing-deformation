3f1d753a0308f2a2ac843a82aad3e3ee1daf65e2
jnyao <43320994+jnyao@users.noreply.github.com>
2018-10-25 21:01:56 -0400

    Update arap_single_iteration.cpp

--- src/arap_single_iteration.cpp
+++ src/arap_single_iteration.cpp
@@ -1,38 +1,31 @@
-#include "biharmonic_precompute.h"
+#include "arap_single_iteration.h"
+#include "igl/polar_svd3x3.h"
 #include <igl/min_quad_with_fixed.h>
-#include <igl/cotmatrix.h>
-#include <igl/massmatrix.h>
-#include<Eigen/SparseCholesky>
 #include <iostream>
 
-using namespace Eigen;
 using namespace std;
 
-void biharmonic_precompute(
-  const Eigen::MatrixXd & V,
-  const Eigen::MatrixXi & F,
-  const Eigen::VectorXi & b,
-  igl::min_quad_with_fixed_data<double> & data)
+void arap_single_iteration(
+  const igl::min_quad_with_fixed_data<double> & data,
+  const Eigen::SparseMatrix<double> & K,
+  const Eigen::MatrixXd & bc,
+  Eigen::MatrixXd & U)
 {
   // REPLACE WITH YOUR CODE
-  data.n = V.rows();
-  Eigen::SparseMatrix<double> L(V.rows(), V.cols());
-  igl::cotmatrix(V, F, L);
-  Eigen::SparseMatrix<double> M(V.rows(), V.cols());
-  igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_DEFAULT, M);
+	// Local step
+	Eigen::MatrixXd R(3*K.rows(), 3);
+	const Eigen::MatrixXd C = K.transpose() * U;
+	// Update each Rk
+	Eigen::Matrix<double, 3, 3> Ck;
+	Eigen::Matrix<double, 3, 3> Rk;
+	for (int k = 0; k < K.rows(); k++) {
+		Ck = C.block(3 * k, 0, 3, 3);
+		igl::polar_svd3x3(Ck, Rk);
+		R.block(3 * k, 0, 3, 3) = Rk;
+	}
 
-  // Calculate M^(-1) * L
-  Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> solver;
-  solver.compute(M);
-  Eigen::SparseMatrix<double> Q1 = solver.solve(L);
-
-  // Calculate L^T * M^(-1) * L
-  Eigen::SparseMatrix<double> Q = L.transpose() * Q1;
-
-  // Precompute
-  Eigen::SparseMatrix<double> B(Q.rows(), Q.cols());
-  B.setZero();
-  Eigen::SparseMatrix<double> Aeq;
-  igl::min_quad_with_fixed_precompute(Q, b, Aeq, true, data);
-
+	// Global step
+	Eigen::MatrixXd B = K * R;
+	Eigen::MatrixXd Beq;
+	igl::min_quad_with_fixed_solve(data, B, bc, Beq, U);
 }
