ae67825eddb9f162628df90ca091417c06ebf66a
jnyao <43320994+jnyao@users.noreply.github.com>
2018-10-25 21:00:57 -0400

    Update arap_single_iteration.cpp

--- src/arap_single_iteration.cpp
+++ src/arap_single_iteration.cpp
@@ -1,4 +1,9 @@
 #include "arap_single_iteration.h"
+#include "igl/polar_svd3x3.h"
+#include <igl/min_quad_with_fixed.h>
+#include <iostream>
+
+using namespace std;
 
 void arap_single_iteration(
   const igl::min_quad_with_fixed_data<double> & data,
@@ -7,4 +12,20 @@ void arap_single_iteration(
   Eigen::MatrixXd & U)
 {
   // REPLACE WITH YOUR CODE
+	// Local step
+	Eigen::MatrixXd R(3*K.rows(), 3);
+	const Eigen::MatrixXd C = K.transpose() * U;
+	// Update each Rk
+	Eigen::Matrix<double, 3, 3> Ck;
+	Eigen::Matrix<double, 3, 3> Rk;
+	for (int k = 0; k < K.rows(); k++) {
+		Ck = C.block(3 * k, 0, 3, 3);
+		igl::polar_svd3x3(Ck, Rk);
+		R.block(3 * k, 0, 3, 3) = Rk;
+	}
+
+	// Global step
+	Eigen::MatrixXd B = K * R;
+	Eigen::MatrixXd Beq;
+	igl::min_quad_with_fixed_solve(data, B, bc, Beq, U);
 }
